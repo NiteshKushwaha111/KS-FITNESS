d251f9d8b6d945187a4b32a65eec1946d5e192c6
NiteshKushwaha111 <107803664+NiteshKushwaha111@users.noreply.github.com>
2024-06-07 23:10:09 +0530

    UI-All-pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { AboutComponent } from './modules/about/about.component';
+import { FeaturesComponent } from './modules/features/features.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { PlanPricingComponent } from './modules/plan-pricing/plan-pricing.component';
 
-export const routes: Routes = [];
+// export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'about', component: AboutComponent},
+    {path:'features', component: FeaturesComponent},
+    {path:'contact', component: ContactComponent},
+    {path:'plan-pricing', component: PlanPricingComponent},
+  ];
---
+++ src/app/modules/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports : [RouterModule],
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent {
+
+}
